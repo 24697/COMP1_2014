8cda57a2278479dc7aca40cdbbd411691a6729d3
24697 <Fuck.Off@Please.GoAway>
2014-04-08 11:47:42 +0100

    working on sheet 1
    Compleated the date time questions and is working

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -5,6 +5,7 @@
 # version 2 edited 06/03/2014
 
 import random
+import time
 
 NO_OF_RECENT_SCORES = 3
 
@@ -17,6 +18,7 @@ class TRecentScore():
   def __init__(self):
     self.Name = ''
     self.Score = 0
+    self.Date = time.strptime('01/01/70','%d/%m/%y')
 
 Deck = [None]
 RecentScores = [None]
@@ -172,14 +174,15 @@ def ResetRecentScores(RecentScores):
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
     RecentScores[Count].Name = ''
     RecentScores[Count].Score = 0
+    RecentScores[Count].Date = time.strptime('01/01/70','%d/%m/%y')
 
 def DisplayRecentScores(RecentScores):
   print()
   print('Recent Scores: ')
   print()
-  print('{0:<10} {1:>10}'.format('Name:','Score:'))
-  for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    print('{0:<10} {1:>5}'.format(RecentScores[Count].Name, RecentScores[Count].Score))
+  print('{0:<10} {1:>10} {2:>10}'.format('Name:','Score:','Date:'))
+  for Count in range(1, NO_OF_RECENT_SCORES + 1):    
+    print('{0:<10} {1:>5} {2:>11}/{3:>0}/{4:>0}'.format(RecentScores[Count].Name, RecentScores[Count].Score, RecentScores[Count].Date.tm_mday, RecentScores[Count].Date.tm_mon, RecentScores[Count].Date.tm_year))
   print()
   print('Press the Enter key to return to the main menu')
   input()
@@ -213,6 +216,9 @@ def UpdateRecentScores(RecentScores, Score):
       Count = NO_OF_RECENT_SCORES
     RecentScores[Count].Name = PlayerName
     RecentScores[Count].Score = Score
+    hold1 = str(input('Please enter a date in the from DD/MM/YY: '))
+    hold2 = time.strptime(hold1,'%d/%m/%y')
+    RecentScores[Count].Date = hold2
   else:
     pass
 
--- time test.py
+++ time test.py
@@ -1,5 +1,10 @@
 import time
-help1 = True
-while help1 == True:
-    now_time = time.asctime( time.localtime(time.time()))
-    print(now_time)
+date = str(input('Please enter a date in the from DD/MM/YY: '))
+date2 = time.strptime(date,'%d/%m/%y')
+#Refer to https://docs.python.org/2/library/time.html#time.strptime for datetime syntax
+hold = date2.tm_year
+print(hold)
+hold = date2.tm_mon
+print(hold)
+hold = date2.tm_mday
+print(hold)
